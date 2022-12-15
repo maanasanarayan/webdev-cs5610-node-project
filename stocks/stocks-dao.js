05571e30750c6ed344ac496a4d8ba2be1ddbec6b
maanasanarayan <mnsnryn@gmail.com>
2022-12-15 18:45:43 +0530

    Create stock fixes: duplicate stock creation

--- stocks/stocks-dao.js
+++ stocks/stocks-dao.js
@@ -1,17 +1,22 @@
 import moviesModel from "./stocks-model.js";
 import stocksModel from "./stocks-model.js";
 
-
 export const findAllStocks = async () => {
-    const movies = await stocksModel.find()
-    return movies
-}
+  const stocks = await stocksModel.find();
+  return stocks;
+};
+
+export const findStockBySymbol = async (symbol) => {
+  const stock = await stocksModel.findOne({ symbol });
+  return stock;
+};
+
 export const createStock = async (stock) => {
-    const actualInsertedStock = await moviesModel.create(stock)
-    return actualInsertedStock
-}
+  const actualInsertedStock = await moviesModel.create(stock);
+  return actualInsertedStock;
+};
 export const deleteStock = async (sid) => {
-    const status = await moviesModel.deleteOne({_id: sid})
-    return status
-}
-export const updateMovie = () => {}
+  const status = await moviesModel.deleteOne({ _id: sid });
+  return status;
+};
+export const updateMovie = () => {};
